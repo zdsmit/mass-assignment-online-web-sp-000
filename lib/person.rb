75397dbe9fdab8079fda4559394e1d77e51de0e6
Zach  Smith <zdsmith001@gmail.com>
2019-09-03 23:06:06 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,10 @@
 class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize
+  def initialize(attributes)
+    
   end
 
 end

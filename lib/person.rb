247b6d40792ed13553ab6c432b772f390182e53e
Zach  Smith <zdsmith001@gmail.com>
2019-09-03 23:21:21 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,10 @@
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
-
   def initialize(attributes=nil)
     if attributes
-      attributes.each {|key, value| self.send(("#{key=}"), value)}
+      attributes.each do |k,v|
+        self.send("#{k}=", v)
+      end
     end
   end
-
 end
